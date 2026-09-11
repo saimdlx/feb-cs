ed5c5015df2630ee0df9cc958d62b1e9afd56c0c
saimdlx <1629saim@gmail.com>
2026-09-11 01:13:33 -0700

    (add) finalizing state logic

--- bms-state/main.cpp
+++ bms-state/main.cpp
@@ -41,12 +41,12 @@ struct BatteryVitals {
     //constants from manual, static because we don't want them reevaluated or changed per program run, and because syntax lol
     const static float MAX_VOLT = 4.2;
     const static float MIN_VOLT = 2.8;
-    const static float MIN_TEMP_DISCHARGE;
-    const static float MAX_TEMP_DISCHARGE;
     const static float MIN_TEMP_CHARGE = 0;
     const static float MAX_TEMP_CHARGE = 45;
     const static float MIN_TEMP_DISCHARGE = -20;
     const static float MAX_TEMP_DISCHARGE = 60;
+    const static float PRECHARGER_TIMEOUT = 2000;
+    const static float CURRENT_DROP_THRESHOLD = 0.5;
 
     //vital variables
     float curr_volt;
@@ -61,7 +61,10 @@ struct BatteryVitals {
 
     //inputs
     bool start_cmd;
+    bool stop_cmd;
     bool charge_cmd;
+    bool clear_cmd;
+
     //hard stop diagnostics, stored as an integer to account for error codes and such, ideally paired with actual bits?
     int fault_err;
 };
@@ -71,17 +74,22 @@ struct BatteryVitals {
     Ideally, this is paired with some sort of a heartbeat system (a fast one) so to not miss crucial updates on the batteries vitals.
     The inputs are the current state, "currState", and the vitals of our car by reference (evaluate the struct).
 */
-BatteryState transitionLogic(BatteryState currState, BatteryVitals& currVitals){
-
-    //Safety precheck, if the voltage doesn't adhere to the max or min, return a fault in voltage.
+BatteryState transitionLogic(BatteryState currState, const BatteryVitals& currVitals){
+    /*
+        Place global safety checks here, ABSOLUTE checks.
+    */
+    /*
+        Safety precheck, if the voltage doesn't adhere to the max or min, return a fault in voltage.
+    */
     if (currVitals.curr_volt > BatteryVitals::MAX_VOLT || currVitals.curr_volt < BatteryVitals::MIN_VOLT){
         return SOMEFAULT;
     }
-
-    if (currVitals.fault_err > 0){ //assume some error code was present
+    /*
+        If this runs an error code appeared and wasn't cleared.
+    */
+    if (currVitals.fault_err > 0){ 
         return SOMEFAULT;
     }
-
     switch(currState){
         case START:
             return STANDBY;
@@ -92,13 +100,55 @@ BatteryState transitionLogic(BatteryState currState, BatteryVitals& currVitals){
             if (currVitals.charge_cmd){
                 return CHARGE;
             }
+            break;
         case PRECHARGE:
-            
+            if (currVitals.stop_cmd){
+                return STANDBY;
+            }
+            /*
+                If the precharge time and the current time exceed the max timeout, somethings wrong.
+            */
+            if (currVitals.curr_time - currVitals.precharge_time > BatteryVitals::PRECHARGER_TIMEOUT){
+                return SOMEFAULT;
+            }
+
+            /*
+                Per fsae handbook, precharger should go up to 90 percent of the battery voltage ev 5.6.1
+            */
+            if (currVitals.curr_inverter_volt >= currVitals.curr_volt * 0.9){
+                return DRIVE;
+            }
+            /*
+                The precharge state should only be able to return a different state IF the conditions above are procced, otherwise we should stay in precharge
+            */
+            return PRECHARGE;
+        case CHARGE:
+            //
+            if (currVitals.stop_cmd){
+                return STANDBY;
+            }
+            //
+            if (currVitals.curr_volt >= (BatteryVitals::MAX_VOLT - 0.05) && currVitals.curr_current <= BatteryVitals::CURRENT_DROP_THRESHOLD){
+                return STANDBY;
+            }
+            return CHARGE;
+        case DRIVE:
+            if (currVitals.stop_cmd){
+                return STANDBY;
+            }
+            return DRIVE;
+        case SOMEFAULT:
+            /*
+                I think unless we verifiably clear some error command, we're still on somefault mode.
+            */
+            if (currVitals.clear_cmd){
+                return STANDBY;
+            }
+            return SOMEFAULT;
+        case SHUTDOWN:
+            return SHUTDOWN;
             
     }
-
-
-
     return currState;
 }
 
